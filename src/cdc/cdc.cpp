2be6157b6a44f3524f88f4b8d4a8a8bd9189f9bf
bitluni <bitluni@bitluni.net>
2025-10-24 13:16:18 +0200

    cleanup and descriptor changes

--- src/cdc/cdc.cpp
+++ src/cdc/cdc.cpp
@@ -20,9 +20,6 @@
 #define USB_CDC_MODE      1
 #define USB_VENDOR_MODE   0
 
-UINT8 usb_cdc_mode_trans_en = 0;   //cdcģʽ�������ϴ�ʹ��
-UINT8 usb_ven_mode_trans_en = 0;   //����ģʽ�������ϴ�ʹ��
-
 const UINT8 TAB_USB_CDC_DEV_DES[18] =
 {
 	0x12,
@@ -42,8 +39,7 @@ const UINT8 TAB_USB_CDC_DEV_DES[18] =
 	0x01
 };
 
-
-/* ���������� */
+/*
 const UINT8 TAB_USB_CDC_CFG_DES[ ] =
 {
 	0x09,0x02,0x43,0x00,0x02,0x01,0x00,0x80,0x30,
@@ -60,13 +56,109 @@ const UINT8 TAB_USB_CDC_CFG_DES[ ] =
 
 	0x07,0x05,0x01,0x02,0x40,0x00,0x00,
 	0x07,0x05,0x81,0x02,0x40,0x00,0x00,
+};*/
+
+// -----------------------------------------------------------------------------
+// USB CDC-ACM Configuration Descriptor
+// Two interfaces: 
+//   Interface 0 = Communication (Control)
+//   Interface 1 = Data
+// Three endpoints total: 
+//   EP4 IN  (Interrupt, notifications)
+//   EP1 OUT (Bulk, data from host)
+//   EP1 IN  (Bulk, data to host)
+// -----------------------------------------------------------------------------
+const uint8_t CDC_ConfigDescriptor[] = 
+{
+    // --- Configuration Descriptor ---
+    0x09,                       // bLength
+    0x02,                       // bDescriptorType = CONFIGURATION
+    0x43, 0x00,                 // wTotalLength = 67 bytes
+    0x02,                       // bNumInterfaces = 2
+    0x01,                       // bConfigurationValue = 1
+    0x00,                       // iConfiguration = none
+    0x80,                       // bmAttributes = Bus powered
+    0x30,                       // bMaxPower = 48 mA (value * 2)
+
+    // --- Interface 0: Communication Interface (CDC Control) ---
+    0x09,                       // bLength
+    0x04,                       // bDescriptorType = INTERFACE
+    0x00,                       // bInterfaceNumber = 0
+    0x00,                       // bAlternateSetting
+    0x01,                       // bNumEndpoints = 1 (Interrupt IN)
+    0x02,                       // bInterfaceClass = Communications (CDC)
+    0x02,                       // bInterfaceSubClass = Abstract Control Model (ACM)
+    0x01,                       // bInterfaceProtocol = AT commands (V.25ter)
+    0x00,                       // iInterface = none
+
+    // --- Header Functional Descriptor ---
+    0x05,                       // bFunctionLength
+    0x24,                       // bDescriptorType = CS_INTERFACE
+    0x00,                       // bDescriptorSubtype = Header
+    0x10, 0x01,                 // bcdCDC = 1.10
+
+    // --- Abstract Control Management Functional Descriptor ---
+    0x04,                       // bFunctionLength
+    0x24,                       // bDescriptorType = CS_INTERFACE
+    0x02,                       // bDescriptorSubtype = Abstract Control Management
+    0x02,                       // bmCapabilities = Supports Set/Get_Line_Coding & Set_Control_Line_State
+
+    // --- Union Functional Descriptor ---
+    0x05,                       // bFunctionLength
+    0x24,                       // bDescriptorType = CS_INTERFACE
+    0x06,                       // bDescriptorSubtype = Union
+    0x00,                       // bMasterInterface = 0 (Communication)
+    0x01,                       // bSlaveInterface0 = 1 (Data)
+
+    // --- Call Management Functional Descriptor (fixed) ---
+    0x05,                       // bFunctionLength
+    0x24,                       // bDescriptorType = CS_INTERFACE
+    0x01,                       // bDescriptorSubtype = Call Management
+    0x01,                       // bmCapabilities = Device handles call management itself
+    0x01,                       // bDataInterface = 1 (Data interface)
+
+    // --- Endpoint: Interrupt IN (Notification, EP4 IN) ---
+    0x07,                       // bLength
+    0x05,                       // bDescriptorType = ENDPOINT
+    0x84,                       // bEndpointAddress = IN endpoint 4 (0x80 | 4)
+    0x03,                       // bmAttributes = Interrupt
+    0x08, 0x00,                 // wMaxPacketSize = 8 bytes
+    0x01,                       // bInterval = 1 ms
+
+    // --- Interface 1: Data Interface ---
+    0x09,                       // bLength
+    0x04,                       // bDescriptorType = INTERFACE
+    0x01,                       // bInterfaceNumber = 1
+    0x00,                       // bAlternateSetting
+    0x02,                       // bNumEndpoints = 2 (Bulk IN + Bulk OUT)
+    0x0A,                       // bInterfaceClass = Data
+    0x00,                       // bInterfaceSubClass = none
+    0x00,                       // bInterfaceProtocol = none
+    0x00,                       // iInterface = none
+
+    // --- Endpoint: Bulk OUT (EP1 OUT) ---
+    0x07,                       // bLength
+    0x05,                       // bDescriptorType = ENDPOINT
+    0x01,                       // bEndpointAddress = OUT endpoint 1
+    0x02,                       // bmAttributes = Bulk
+    0x40, 0x00,                 // wMaxPacketSize = 64 bytes
+    0x00,                       // bInterval = N/A (Bulk)
+
+    // --- Endpoint: Bulk IN (EP1 IN) ---
+    0x07,                       // bLength
+    0x05,                       // bDescriptorType = ENDPOINT
+    0x81,                       // bEndpointAddress = IN endpoint 1
+    0x02,                       // bmAttributes = Bulk
+    0x40, 0x00,                 // wMaxPacketSize = 64 bytes
+    0x00,                       // bInterval = N/A (Bulk)
 };
+
 
 const UINT8 My_QueDescr[ ] = { 0x0A, 0x06, 0x00, 0x02, 0xFF, 0x00, 0xFF, 0x40, 0x01, 0x00 };
 
 UINT8 TAB_CDC_LINE_CODING[ ]  =
 {
-	0x85, /* baud rate*/
+	0x85,	/* baud rate*/
 	0x20,
 	0x00,
 	0x00,
@@ -77,9 +169,9 @@ UINT8 TAB_CDC_LINE_CODING[ ]  =
 
 const UINT8 TAB_USB_LID_STR_DES[ ] = { 0x04, 0x03, 0x09, 0x04 };
 
-const UINT8 USB_DEV_PARA_CDC_SERIAL_STR[] =     "videoshnipsel";
-const UINT8 USB_DEV_PARA_CDC_PRODUCT_STR[] =    "bitluni's CDC";
-const UINT8 USB_DEV_PARA_CDC_MANUFACTURE_STR[] = "bituni";
+const UINT8 USB_DEV_PARA_CDC_SERIAL_STR[] =			"videoshnipsel";
+const UINT8 USB_DEV_PARA_CDC_PRODUCT_STR[] =		"bitluni's CDC";
+const UINT8 USB_DEV_PARA_CDC_MANUFACTURE_STR[] =	"bituni";
 
 typedef struct DevInfo
 {
@@ -96,12 +188,12 @@ UINT8 SetupReqCode, SetupLen;
 
 __aligned(4) UINT8  Ep0Buffer[MAX_PACKET_SIZE];
 __aligned(4) UINT8  Ep1Buffer[MAX_PACKET_SIZE];     //IN
-__aligned(4) UINT8  Ep2Buffer[2*MAX_PACKET_SIZE];   //OUT & IN
-__aligned(4) UINT8  Ep3Buffer[2*MAX_PACKET_SIZE];   //OUT & IN
+__aligned(4) UINT8  Ep2Buffer[2 * MAX_PACKET_SIZE];   //OUT & IN
+__aligned(4) UINT8  Ep3Buffer[2 * MAX_PACKET_SIZE];   //OUT & IN
 
 typedef struct __PACKED _LINE_CODE
 {
-	UINT32  BaudRate;
+	UINT32 BaudRate;
 	UINT8 StopBits;
 	UINT8 ParityType;
 	UINT8 DataBits;
@@ -109,65 +201,33 @@ typedef struct __PACKED _LINE_CODE
 
 LINE_CODE Uart0Para;
 
-#define CH341_REG_NUM     10
-UINT8 CH341_Reg_Add[CH341_REG_NUM];
-UINT8 CH341_Reg_val[CH341_REG_NUM];
-
-
-UINT8 VENSer0ParaChange = 0;
-UINT8 VENSer0SendFlag = 0;
-
-UINT8 UART0_RTS_Val = 0; //��� ��ʾDTE����DCE��������
-UINT8 UART0_DTR_Val = 0; //��� �����ն˾���
-UINT8 UART0_OUT_Val = 0; //�Զ���modem�źţ�CH340�ֲᣩ
-
-UINT8 UART0_DCD_Val = 0;
-UINT8 UART0_DCD_Change = 0;
-
-UINT8 UART0_RI_Val = 0;
-UINT8 UART0_RI_Change = 0;
-
-UINT8 UART0_DSR_Val = 0;
-UINT8 UART0_DSR_Change = 0;
-
-UINT8 UART0_CTS_Val = 0;
-UINT8 UART0_CTS_Change = 0;
-
-/* CDC���õĴ��� */
 UINT8 CDCSetSerIdx = 0;
 UINT8 CDCSer0ParaChange = 0;
 
-typedef struct _USB_SETUP_REQ_ {
-		UINT8 bRequestType;
-		UINT8 bRequest;
-		UINT8 wValueL;
-		UINT8 wValueH;
-		UINT8 wIndexL;
-		UINT8 wIndexH;
-		UINT8 wLengthL;
-		UINT8 wLengthH;
+typedef struct _USB_SETUP_REQ_ 
+{
+	UINT8 bRequestType;
+	UINT8 bRequest;
+	UINT8 wValueL;
+	UINT8 wValueH;
+	UINT8 wIndexL;
+	UINT8 wIndexH;
+	UINT8 wLengthL;
+	UINT8 wLengthH;
 } USB_SETUP_REQ_t;
 
 #define UsbSetupBuf     ((USB_SETUP_REQ_t *)Ep0Buffer) //USB_SETUP_REQ_t USB_SETUP_REQ_t
 
-/* USB���涨�壬���ж˵�ȫ������ */
-/* �˵�1 -- IN״̬ */
 UINT8 Ep1DataINFlag = 0;
-
-/* �˵�1�´����� */
 UINT8 Ep1DataOUTFlag = 0;
 UINT8 Ep1DataOUTLen = 0;
 __aligned(4) UINT8 Ep1OUTDataBuf[MAX_PACKET_SIZE];
 
-/* �˵�2 -- IN״̬ */
 UINT8 Ep2DataINFlag = 0;
-
-/* �˵�2�´����� */
 UINT8 Ep2DataOUTFlag = 0;
 UINT8 Ep2DataOUTLen = 0;
 __aligned(4) UINT8 Ep2OUTDataBuf[MAX_PACKET_SIZE];
 
-/* ����USB�жϵ�״̬ ->�ĳɼ���Ĳ�����ʽ */
 #define USB_IRQ_FLAG_NUM     4
 
 UINT8 usb_irq_w_idx = 0;
@@ -180,7 +240,6 @@ volatile UINT8 usb_irq_flag[USB_IRQ_FLAG_NUM];
 UINT8 cdc_uart_sta_trans_step = 0;
 UINT8 ven_ep1_trans_step = 0;
 
-/* �˵�0ö���ϴ�֡���� */
 UINT8 ep0_send_buf[256];
 
 /**********************************************************/
@@ -200,14 +259,13 @@ const UINT8 *pDescr;
 #define HAL_UART_TWO_STOP_BITS               2
 
 /* Parity settings */
-#define HAL_UART_NO_PARITY                   0x00                              //��У��
-#define HAL_UART_ODD_PARITY                  0x01                              //��У��
-#define HAL_UART_EVEN_PARITY                 0x02                              //żУ��
-#define HAL_UART_MARK_PARITY                 0x03                              //��1 mark
-#define HAL_UART_SPACE_PARITY                0x04                              //�հ�λ space
+#define HAL_UART_NO_PARITY                   0x00
+#define HAL_UART_ODD_PARITY                  0x01
+#define HAL_UART_EVEN_PARITY                 0x02
+#define HAL_UART_MARK_PARITY                 0x03
+#define HAL_UART_SPACE_PARITY                0x04
 
 
-/* �˵�״̬���ú��� */
 void USBDevEPnINSetStatus(UINT8 ep_num, UINT8 type, UINT8 sta);
 
 /* endpoints enumeration */
@@ -221,7 +279,6 @@ void USBDevEPnINSetStatus(UINT8 ep_num, UINT8 type, UINT8 sta);
 #define ENDP_TYPE_IN                    0x00                                    /* ENDP is IN Type */
 #define ENDP_TYPE_OUT                   0x01                                    /* ENDP is OUT Type */
 
-/* �˵�Ӧ��״̬���� */
 /* OUT */
 #define OUT_ACK                         0
 #define OUT_TIMOUT                      1
@@ -243,63 +300,73 @@ void USBDevEPnINSetStatus(UINT8 ep_num, UINT8 type, UINT8 sta);
 #define DEF_BIT_USB_SUPD                0x40                                    /* USB���߹����־ */
 #define DEF_BIT_USB_HS                  0x80                                    /* USB���١�ȫ�ٱ�־ */
 
-/* �жϴ������� */
 __attribute__((interrupt("WCH-Interrupt-fast")))
 __attribute__((section(".highcode")))
 void USB_IRQHandler(void)
 {
-	UINT8   j;
-
 	if(R8_USB_INT_FG & RB_UIF_TRANSFER)
 	{
-		/* ��setup������ */
-		if((R8_USB_INT_ST & MASK_UIS_TOKEN) != MASK_UIS_TOKEN){     // �ǿ���
-			/* ֱ��д�� */
+		if((R8_USB_INT_ST & MASK_UIS_TOKEN) != MASK_UIS_TOKEN)
+		{	
 			usb_irq_flag[usb_irq_w_idx] = 1;
 			usb_irq_pid[usb_irq_w_idx]  = R8_USB_INT_ST;  //& 0x3f;//(0x30 | 0x0F);
 			usb_irq_len[usb_irq_w_idx]  = R8_USB_RX_LEN;
 
-			switch(usb_irq_pid[usb_irq_w_idx]& 0x3f){   //������ǰ�Ķ˵�
-				case UIS_TOKEN_OUT | 2:{
-					if( R8_USB_INT_FG & RB_U_TOG_OK ){   //��ͬ�������ݰ�������
+			switch(usb_irq_pid[usb_irq_w_idx]& 0x3f)
+			{
+				case UIS_TOKEN_OUT | 2:
+				{
+					if( R8_USB_INT_FG & RB_U_TOG_OK )
+					{
 						R8_UEP2_CTRL ^=  RB_UEP_R_TOG;
 						R8_UEP2_CTRL = (R8_UEP2_CTRL & 0xf3) | 0x08; //OUT_NAK
-						/* �������� */
-						for(j=0; j<(MAX_PACKET_SIZE/4); j++)
-							((UINT32 *)Ep2OUTDataBuf)[j] = ((UINT32 *)Ep2Buffer)[j];
+						for(int i = 0; i < (MAX_PACKET_SIZE / 4); i++)
+							((UINT32 *)Ep2OUTDataBuf)[i] = ((UINT32 *)Ep2Buffer)[i];
 					}
 					else usb_irq_flag[usb_irq_w_idx] = 0;
-				}break;
-				case UIS_TOKEN_IN | 2:{ //endpoint 2# �����˵��ϴ����
+					break;
+				}
+				case UIS_TOKEN_IN | 2:
+				{ 
+					//endpoint 2# 
 					R8_UEP2_CTRL ^=  RB_UEP_T_TOG;
 					R8_UEP2_CTRL = (R8_UEP2_CTRL & 0xfc) | IN_NAK; //IN_NAK
-				}break;
-				case UIS_TOKEN_OUT | 1:{
-					if( R8_USB_INT_FG & RB_U_TOG_OK ){   //��ͬ�������ݰ�������
+					break;
+				}
+				case UIS_TOKEN_OUT | 1:
+				{
+					if( R8_USB_INT_FG & RB_U_TOG_OK )
+					{
 						R8_UEP1_CTRL ^=  RB_UEP_R_TOG;
 						R8_UEP1_CTRL = (R8_UEP1_CTRL & 0xf3) | 0x08; //OUT_NAK
-						/* �������� */
-						for(j=0; j<(MAX_PACKET_SIZE/4); j++)
-							((UINT32 *)Ep1OUTDataBuf)[j] = ((UINT32 *)Ep1Buffer)[j];
+						for(int i = 0; i < (MAX_PACKET_SIZE / 4); i++)
+							((UINT32 *)Ep1OUTDataBuf)[i] = ((UINT32 *)Ep1Buffer)[i];
 					}
 					else usb_irq_flag[usb_irq_w_idx] = 0;
-				}break;
-				case UIS_TOKEN_IN | 1:{  //endpoint 1# �����˵��ϴ����
+					break;
+				}
+				case UIS_TOKEN_IN | 1:
+				{  //endpoint 1#
 					R8_UEP1_CTRL ^=  RB_UEP_T_TOG;
 					R8_UEP1_CTRL = (R8_UEP1_CTRL & 0xfc) | IN_NAK; //IN_NAK
-				}break;
-				case UIS_TOKEN_OUT | 0:{    // endpoint 0
-					if( R8_USB_INT_FG & RB_U_TOG_OK )   //��ͬ�������ݰ�������
+					break;
+				}
+				case UIS_TOKEN_OUT | 0:
+				{    // endpoint 0
+					if( R8_USB_INT_FG & RB_U_TOG_OK )
 						R8_UEP0_CTRL = (R8_UEP0_CTRL & 0xf3) | 0x08; //OUT_NAK
 					else usb_irq_flag[usb_irq_w_idx] = 0;
-				}break;
-				case UIS_TOKEN_IN | 0:{  //endpoint 0
+					break;
+				}
+				case UIS_TOKEN_IN | 0:
+				{  //endpoint 0
 					R8_UEP0_CTRL = (R8_UEP0_CTRL & 0xfc) | IN_NAK; //IN_NAK
-				}break;
+					break;
+				}
 			}
 
-			/* �е���һ��д��ַ */
-			if(usb_irq_flag[usb_irq_w_idx]){
+			if(usb_irq_flag[usb_irq_w_idx])
+			{
 				usb_irq_w_idx++;
 				if(usb_irq_w_idx >= USB_IRQ_FLAG_NUM) usb_irq_w_idx = 0;
 			}
@@ -307,17 +374,13 @@ void USB_IRQHandler(void)
 			R8_USB_INT_FG = RB_UIF_TRANSFER;
 		}
 
-		/* setup������ */
 		if(R8_USB_INT_ST & RB_UIS_SETUP_ACT){
-			/* ��֮ǰ�Ĵ����ӹ� -- UIS_TOKEN_SETUP */
-			/* ֱ��д�� */
+			//UIS_TOKEN_SETUP
 			usb_irq_flag[usb_irq_w_idx] = 1;
-			usb_irq_pid[usb_irq_w_idx]  = UIS_TOKEN_SETUP | 0;  //����֮ǰ�ı�־
+			usb_irq_pid[usb_irq_w_idx]  = UIS_TOKEN_SETUP | 0;
 			usb_irq_len[usb_irq_w_idx]  = 8;
-			/* �е���һ��д��ַ */
 			usb_irq_w_idx++;
 			if(usb_irq_w_idx >= USB_IRQ_FLAG_NUM) usb_irq_w_idx = 0;
-
 			R8_USB_INT_FG = RB_UIF_TRANSFER;
 		}
 	}
@@ -326,30 +389,17 @@ void USB_IRQHandler(void)
 UINT8 Ep4DataINFlag;
 UINT8 Ep3DataINFlag;
 
-//UINT8 Ep1DataOUTLen ;
-//UINT8 Ep1DataOUTFlag ;
-
 UINT8 Ep3DataOUTFlag = 0;
 UINT8 Ep4DataOUTFlag = 0;
 
-/* CH341��ص�����֡ */
-#define DEF_VEN_DEBUG_READ              0X95
-#define DEF_VEN_DEBUG_WRITE             0X9A
-#define DEF_VEN_UART_INIT             0XA1
-#define DEF_VEN_UART_M_OUT              0XA4
-#define DEF_VEN_BUF_CLEAR             0XB2
-#define DEF_VEN_I2C_CMD_X             0X54
-#define DEF_VEN_DELAY_MS              0X5E
-#define DEF_VEN_GET_VER                 0X5F
-
 //  3.1 Requests---Abstract Control Model
-#define DEF_SEND_ENCAPSULATED_COMMAND  0x00
-#define DEF_GET_ENCAPSULATED_RESPONSE  0x01
-#define DEF_SET_COMM_FEATURE           0x02
-#define DEF_GET_COMM_FEATURE           0x03
-#define DEF_CLEAR_COMM_FEATURE         0x04
-#define DEF_SET_LINE_CODING          0x20   // Configures DTE rate, stop-bits, parity, and number-of-character
-#define DEF_GET_LINE_CODING          0x21   // This request allows the host to find out the currently configured line coding.
+#define DEF_SEND_ENCAPSULATED_COMMAND  	0x00
+#define DEF_GET_ENCAPSULATED_RESPONSE  	0x01
+#define DEF_SET_COMM_FEATURE           	0x02
+#define DEF_GET_COMM_FEATURE           	0x03
+#define DEF_CLEAR_COMM_FEATURE         	0x04
+#define DEF_SET_LINE_CODING          	0x20   // Configures DTE rate, stop-bits, parity, and number-of-character
+#define DEF_GET_LINE_CODING          	0x21   // This request allows the host to find out the currently configured line coding.
 //#define DEF_SET_CTL_LINE_STE         0X22   // This request generates RS-232/V.24 style control signals.
 #define DEF_SET_CONTROL_LINE_STATE     0x22
 #define DEF_SEND_BREAK                 0x23
@@ -399,7 +449,6 @@ void USB_IRQProcessHandler( void )
 						//CH341�������·�
 						Ep2DataOUTFlag = 1;
 						Ep2DataOUTLen = len;
-						VENSer0SendFlag = 1;
 						PFIC_DisableIRQ(USB_IRQn);
 						R8_UEP2_CTRL = R8_UEP2_CTRL & 0xf3; //OUT_ACK
 						PFIC_EnableIRQ(USB_IRQn);
@@ -418,10 +467,8 @@ void USB_IRQProcessHandler( void )
 					sendCDCData(Ep1OUTDataBuf, len);
 					//SendUSBData(Ep1OUTDataBuf, len); //TODO this is CDC recv
 
-					//CH341�������·�
 					Ep1DataOUTFlag = 1;
 					Ep1DataOUTLen = len;
-					VENSer0SendFlag = 1;
 					PFIC_DisableIRQ(USB_IRQn);
 					R8_UEP1_CTRL = R8_UEP1_CTRL & 0xf3; //OUT_ACK
 					PFIC_EnableIRQ(USB_IRQn);
@@ -477,7 +524,7 @@ void USB_IRQProcessHandler( void )
 										{
 											switch(UsbSetupBuf->wValueL)
 											{
-												case 0:  //����������
+												case 0:
 												{
 													pDescr = (PUINT8)( &TAB_USB_LID_STR_DES[0] );
 													len = sizeof( TAB_USB_LID_STR_DES );
@@ -493,7 +540,6 @@ void USB_IRQProcessHandler( void )
 													UINT8 *manu_str;
 													UINT8 tmp;
 
-													/* ȡ���� */
 													if(UsbSetupBuf->wValueL == 1)
 														manu_str = (UINT8 *)USB_DEV_PARA_CDC_MANUFACTURE_STR;
 													else if(UsbSetupBuf->wValueL == 2)
@@ -516,7 +562,7 @@ void USB_IRQProcessHandler( void )
 													break;
 												}
 												default:
-													len = 0xFF;    // ��֧�ֵ�����������
+													len = 0xFF;
 													break;
 											}
 											break;
@@ -643,11 +689,8 @@ void USB_IRQProcessHandler( void )
 									}
 									case DEF_SET_CONTROL_LINE_STATE:  /* SET_CONTROL_LINE_STATE */
 									{
-										/*UINT8  carrier_sta;
-										UINT8  present_sta;
-										carrier_sta = Ep0Buffer[2] & (1<<1);   //RTS״̬
-										present_sta = Ep0Buffer[2] & (1<<0);   //DTR״̬
-										*/
+										//Ep0Buffer[2] & (1<<1);   //RTS
+										//Ep0Buffer[2] & (1<<0);   //DTR
 										len = 0;
 										break;
 									}
@@ -725,27 +768,6 @@ void USB_IRQProcessHandler( void )
 							R8_UEP0_CTRL = RB_UEP_R_TOG | RB_UEP_T_TOG | UEP_R_RES_ACK | UEP_T_RES_ACK;
 							PFIC_EnableIRQ(USB_IRQn);
 						}
-						else if( SetupReqCode ==  DEF_VEN_UART_INIT )  //0XA1
-						{
-							R8_UEP0_T_LEN = len;
-							PFIC_DisableIRQ(USB_IRQn);
-							R8_UEP0_CTRL = RB_UEP_R_TOG | RB_UEP_T_TOG | UEP_R_RES_NAK | UEP_T_RES_ACK;
-							PFIC_EnableIRQ(USB_IRQn);
-						}
-						else if( SetupReqCode ==  DEF_VEN_DEBUG_WRITE )  //0X9A
-						{
-							R8_UEP0_T_LEN = len;
-							PFIC_DisableIRQ(USB_IRQn);
-							R8_UEP0_CTRL = RB_UEP_R_TOG | RB_UEP_T_TOG | UEP_R_RES_NAK | UEP_T_RES_ACK;
-							PFIC_EnableIRQ(USB_IRQn);
-						}
-						else if( SetupReqCode ==  DEF_VEN_UART_M_OUT )  //0XA4
-						{
-							R8_UEP0_T_LEN = len;
-							PFIC_DisableIRQ(USB_IRQn);
-							R8_UEP0_CTRL = RB_UEP_R_TOG | RB_UEP_T_TOG | UEP_R_RES_NAK | UEP_T_RES_ACK;
-							PFIC_EnableIRQ(USB_IRQn);
-						}
 						else if( SetupReqCode ==  DEF_SET_CONTROL_LINE_STATE )  //0x22
 						{
 							PFIC_DisableIRQ(USB_IRQn);
@@ -820,15 +842,6 @@ void USB_IRQProcessHandler( void )
 							R8_USB_DEV_AD = (R8_USB_DEV_AD & RB_UDA_GP_BIT) | (devinf.UsbAddress);
 							PFIC_DisableIRQ(USB_IRQn);
 							R8_UEP0_CTRL = RB_UEP_R_TOG|RB_UEP_T_TOG|UEP_R_RES_NAK | UEP_T_RES_NAK;
-							PFIC_EnableIRQ(USB_IRQn);
-
-							break;
-						}
-						case DEF_VEN_DEBUG_READ:     //0X95
-						case DEF_VEN_GET_VER:         //0X5F
-						{
-							PFIC_DisableIRQ(USB_IRQn);
-							R8_UEP0_CTRL = RB_UEP_R_TOG|RB_UEP_T_TOG|UEP_R_RES_ACK | UEP_T_RES_NAK;
 							PFIC_EnableIRQ(USB_IRQn);
 
 							break;
@@ -1029,25 +1042,8 @@ void InitUSBDevPara(void)
 	Uart0Para.ParityType = HAL_UART_NO_PARITY;
 	Uart0Para.StopBits = HAL_UART_ONE_STOP_BIT;
 
-	VENSer0ParaChange = 0;
-	VENSer0SendFlag = 0;
 	CDCSetSerIdx = 0;
 	CDCSer0ParaChange = 0;
-
-	for(i=0; i<CH341_REG_NUM; i++)
-	{
-		CH341_Reg_Add[i] = 0xff;
-		CH341_Reg_val[i] = 0x00;
-	}
-
-	UART0_DCD_Val = 0;
-	UART0_RI_Val = 0;
-	UART0_DSR_Val = 0;
-	UART0_CTS_Val = 0;
-
-	UART0_RTS_Val = 0;
-	UART0_DTR_Val = 0;
-	UART0_OUT_Val = 0;
 
 	for(i=0; i<USB_IRQ_FLAG_NUM; i++)
 	{
